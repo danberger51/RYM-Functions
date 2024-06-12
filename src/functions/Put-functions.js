3a10599cbcfdaed662979fd09722a0e0aa3dd5bb
DAN BERGER <dan.berger@ict-campus.net>
2024-06-12 09:31:59 +0200

    Das Put funktioniert

---
+++ src/functions/Put-functions.js
@@ -0,0 +1,36 @@
+const { app, output, input } = require("@azure/functions");
+
+const cosmosInput = input.cosmosDB({
+    databaseName: "FilmDatabase",
+    containerName: "Movie-Container",
+    connection: "CosmosDB",
+    qlQuery: "select * from c where c.id = {id}",
+});
+
+const cosmosOutput = output.cosmosDB({
+  databaseName: "FilmDatabase",
+  containerName: "Movie-Container",
+  connection: "CosmosDB",
+  createIfNotExists: true,
+});
+
+app.http("putItem", {
+  methods: ["PUT"],
+  authLevel: "anonymous",
+  extraInputs: [cosmosInput],
+  extraOutputs: [cosmosOutput],
+  route: "items/{id}",
+  handler: async (request, context) => {
+    const item = context.extraInputs.get(cosmosInput);
+    const data = await request.json();
+    console.log("UPDATE", data);
+    data.id = item[0].id;
+
+    context.extraOutputs.set(cosmosOutput, data);
+
+    return {
+      body: JSON.stringify(data),
+      status: 200,
+    };
+  },
+});
