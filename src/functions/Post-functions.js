1500b4fa24f4cb13b8846b8314e8999fb869215e
ebnerju <146084993+ebnerju@users.noreply.github.com>
2024-06-12 14:24:37 +0200

    Update Post-functions.js

--- src/functions/Post-functions.js
+++ src/functions/Post-functions.js
@@ -12,7 +12,7 @@ const cosmosOutput = output.cosmosDB({
 app.http('addMovie', {
     methods: ['POST'],
     authLevel: 'anonymous',
-    route: 'movies/create',
+    route: 'movies',
     extraOutputs: [cosmosOutput],
     handler: async (request, context) => {
         const data = await request.json();
